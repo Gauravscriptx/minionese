dadd418509fe60a778dbe31da540ec61bede86e5
Yogesh Yadav <70842648+Gauravscriptx@users.noreply.github.com>
2021-08-23 22:48:09 +0530

    update app.js

--- app.js
+++ app.js
@@ -1,6 +1,14 @@
 var txtinput = document.querySelector('#txt-input');
 var btnTranslate = document.querySelector('#btn-translate');
 var outputDiv = document.querySelector('#output');
+var serverUrl = 'https://api.funtranslations.com/translate/minion.json';
+function getTranslationUrl(text) {
+  return serverUrl + '?' + 'text=' + text;
+}
+function errorHandler(error) {
+  console.log('error occured', error);
+  alert('something wrong with server');
+}
 
 function clickHandler() {
   outputDiv.innerText = 'bananan' + txtinput.value;
