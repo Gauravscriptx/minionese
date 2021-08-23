f2a0e8501a5c17087fad471a918382dfd00095be
Yogesh Yadav <70842648+Gauravscriptx@users.noreply.github.com>
2021-08-23 21:21:24 +0530

    fix:code

--- app.js
+++ app.js
@@ -1,10 +1,9 @@
-var txTinput = document.querySelector('#txt-input');
+var txtinput = document.querySelector('#txt-input');
 var btnTranslate = document.querySelector('#btn-translate');
 var outputDiv = document.querySelector('#output');
-console.log(txtinput);
-outputDiv.innertxt = 'bananan';
+
 function clickHandler() {
-  console.log('clicked!');
-  console.log('input', txtinput, value);
+  outputDiv.innerText = 'bananan' + txtinput.value;
 }
+
 btnTranslate.addEventListener('click', clickHandler);
