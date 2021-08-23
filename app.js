44b012f1614b0163df633a1383b41ef25cc1d52e
Yogesh Yadav <70842648+Gauravscriptx@users.noreply.github.com>
2021-08-23 20:54:34 +0530

    docs

--- app.js
+++ app.js
@@ -1,3 +1,10 @@
 var txtinput = document.querySelector('#txt-input');
 var btntranslate = document.querySelector('#btn-translate');
+var outputdiv = document.querySelector('#output');
 console.log(txtinput);
+
+function clickhandler() {
+  console.log('clicked!');
+  console.log('input', txtinput, value);
+}
+btntranslate.addEventListener('click', clickhandler);
